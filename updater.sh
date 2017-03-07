f9ea59038161c52d374db6de3a1dc9b6a85bfe04
Pharylon <me@zachshuford.com>
2017-03-07 15:08:54 -0500

    Updated updater

--- updater.sh
+++ updater.sh
@@ -4,9 +4,7 @@ git fetch origin
 #behind=$(git status | grep behind)
 #reslog=$(git log HEAD..origin/master --oneline)
 if (git status | grep behind -q); then
+  /etc/init.d/lamp stop  
   git merge origin/master # completing the pull
-  pkill -f node
-  node main.js
-else
-  #node main.js
+  /etc/init.d/lamp start
 fi
