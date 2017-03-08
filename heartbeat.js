d55f5fca4b9547b20a0d8c08ed935707207e2a8b
Zachary Shuford <zshuford@freightsystemsinc.com>
2017-03-08 12:35:27 -0500

    Fix for potential issue with heartbeat

--- heartbeat.js
+++ heartbeat.js
@@ -25,11 +25,11 @@ function getIp(callback){
       // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
       return;
     }
-    if (alias >= 1) {
+    if (alias >= 1 && iface && iface.address) {
       // this single interface has multiple ipv4 addresses
       console.log(ifname + ':' + alias, iface.address);
       callback(iface.address);
-    } else {
+    } else if (iface && iface.address) {
       // this interface has only one ipv4 adress
       console.log(ifname, iface.address);
       callback(iface.address);
