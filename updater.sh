5bd86f1eb0ca3a6359ebd19eb709f94012eef4aa
Pharylon <me@zachshuford.com>
2017-04-02 08:31:03 -0400

    Updater works on any branch

--- updater.sh
+++ updater.sh
@@ -5,7 +5,7 @@ git fetch origin
 #reslog=$(git log HEAD..origin/master --oneline)
 if (git status | grep behind -q); then
   /etc/init.d/lamp stop  
-  git merge origin/master # completing the pull
+  git merge #origin/master # completing the pull
   /etc/init.d/lamp start
 fi
 /usr/bin/node /home/pi/Projects/lamp/beatit.js
